9f2cf0cd020cfaaea217552af9e06b2f4e8aaee3
omridan159 <omridan159@gmail.com>
2020-12-27 10:54:51 +0200

    work around

--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { setUser, logout,selectUser } from './userSlice';
+import { setUser, logout,setPass,selectUser,selectPass } from './userSlice';
 
 const Login = () => {
     const dispatch = useDispatch();
     const User = useSelector(selectUser);
+    const Pass = useSelector(selectPass);
     
     const loginToApp = () =>{
     dispatch(setUser({
-        user: "Omridan159"
+        user: "Omri"
+    }))
+    }
+    const SetPassword = () =>{
+    dispatch(setPass({
+        pass: "123456"
     }))
     }
 
@@ -20,6 +26,8 @@ const Login = () => {
          {User ? <h2>The User is {User}</h2> : <h2>No one is Sign In</h2>}
          <button onClick={loginToApp}>Log Me In</button>
          <button onClick={() => dispatch(logout())}>Log Me Out</button>
+         {Pass ? <h2>The Pass is {Pass}</h2> : <h2>No Pass</h2>}
+         <button onClick={SetPassword}>SetPassword</button>
       </div>
    );
 };
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -5,6 +5,7 @@ export const userSlice = createSlice({
    name: 'user',
    initialState: {
       user: null,
+      pass: null,
    },
    reducers: {
       setUser: (state, action) => {
@@ -14,17 +15,25 @@ export const userSlice = createSlice({
          // immutable state based off those changes
          state.user = action.payload.user;
       },
+      setPass: (state, action) => {
+         // Redux Toolkit allows us to write "mutating" logic in reducers. It
+         // doesn't actually mutate the state because it uses the Immer library,
+         // which detects changes to a "draft state" and produces a brand new
+         // immutable state based off those changes
+         state.pass = action.payload.pass;
+      },
       logout: (state) => {
          state.user = null;
       },
    },
 });
 
-export const { setUser, logout } = userSlice.actions;
+export const { setUser, logout,setPass } = userSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectUser = (state) => state.user.user;
+export const selectPass = (state) => state.user.pass;
 
 export default userSlice.reducer;
